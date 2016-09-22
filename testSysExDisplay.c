184cc8920fd21ac6dc0d0fa869b841dd56a4c2e5
Simon FLORENTIN <mail@simon-florentin.fr>
2016-09-22 16:57:48 +0200

    Added some testExample and reflexions

---
+++ testSysExDisplay.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main()
+{
+    char sysEx[] = {0xF0, 0x00, 0x00, 0x66, 0x00, 0x12, 0X57, 0x4C, 0x35, 0x31, 0x52, 0x34, 0x39, 0x20, 0xF7};
+    char *ptr;
+    char *readable = sysEx + 6;
+    
+    strtol(readable, &ptr, 10);
+    printf("String part is |%s|\n", ptr);
+    printf("offset |0x%x : %d |", sysEx[6], sysEx[6]);
+    printf("Display |%d|", getDisplay(sysEx));
+
+    return(0);
+}
+
+int getDisplay(char *sysEx){
+    int offset = (sysEx[6] / 7) ;
+        return offset % 8 ;
+}
+

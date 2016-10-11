404017ed60c42b12e770bb29a8a749568467d5a4
Simon FLORENTIN <mail@simon-florentin.fr>
2016-10-11 18:06:58 +0200

    added analyse to display

--- testSysExDisplay.c
+++ testSysExDisplay.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
-
 
 int main()
 {
     char sysEx[] = {0xF0, 0x00, 0x00, 0x66, 0x00, 0x12, 0X57, 0x4C, 0x35, 0x31, 0x52, 0x34, 0x39, 0x20, 0xF7};
     char *ptr;
-    char *readable = sysEx + 6;
+    char *readable = sysEx + 7;
     
     strtol(readable, &ptr, 10);
     printf("String part is |%s|\n", ptr);
@@ -25,12 +25,12 @@ int main()
     };
 
     struct channel oledDisplay1;
-    oledDisplay1.trackName = "Kick";
-    oledDisplay1.assign = "Pn";
 
-
-
-
+    // allocate 7 bytes for 7 digits of the display line
+    oledDisplay1.displayLine1 = malloc(7);
+    // The 7 firsts bytes are used for <hdr>(5) + LCD message(1) + offset(1)
+    strncpy(oledDisplay1.displayLine1, sysEx + 7, 7); // get next 7 bytes (skip the 0xF7 EOSysEx) 
+    printf("\n oledDisplay1.displayLine1 :  |%s|  \n", oledDisplay1.displayLine1);
     
     return(0);
 }

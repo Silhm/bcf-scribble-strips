628b9aedad08e217209aebe4e5a23934072a61d0
Simon FLORENTIN <mail@simon-florentin.fr>
2016-10-17 18:10:20 +0200

    advanced sysEx analyze

--- testSysExDisplay.c
+++ testSysExDisplay.c
@@ -3,9 +3,11 @@
 #include <string.h>
 #include <math.h>
 
+
 int main()
 {
-    char sysEx[] = {0xF0, 0x00, 0x00, 0x66, 0x00, 0x12, 0X57, 0x4C, 0x35, 0x31, 0x52, 0x34, 0x39, 0x20, 0xF7};
+    unsigned char sysEx[] = {0xF0, 0x00, 0x00, 0x66, 0x00, 0x12, 0X57, 0x4C, 0x35, 0x31, 0x52, 0x34, 0x39, 0x20, 0xF7};
+
     char *ptr;
     char *readable = sysEx + 7;
     
@@ -32,6 +34,62 @@ int main()
     strncpy(oledDisplay1.displayLine1, sysEx + 7, 7); // get next 7 bytes (skip the 0xF7 EOSysEx) 
     printf("\n oledDisplay1.displayLine1 :  |%s|  \n", oledDisplay1.displayLine1);
     
+
+    printf("\n -------Handle a big sysEx data -------------- \n");
+    // Received big frame 
+    unsigned char bigSysEx[] = {
+        0xF0, 0x00, 0x00, 0x66, 0x00, 0x12, 0X57, 0x4C, 0x35, 0x31, 0x52, 0x34, 0x39, 0x20, 0xF7, 
+        0xF0, 0x00, 0x00, 0x66, 0x00, 0x12, 0x00, 0x4C, 0x34, 0x32, 0x52, 0x50, 0x59, 0x20, 0xF7,
+        0xF0, 0x00, 0x00, 0x66, 0x00, 0x12, 0x00, 0x4C, 0x35, 0x30, 0x52, 0x50, 0x59, 0x39, 0x39, 0xF7};
+    // function gives us the sysEx size as well
+    int buffersize = 46;
+
+    int i=0;
+    int start=0;
+    int frameCpt = 0;
+
+    //declare frame structure
+    typedef struct {
+       int start;
+       int size;
+       unsigned char *data;
+    } t_frame;
+
+    t_frame *frames;
+    frames = malloc( 3 * sizeof(t_frame) ); // initial size at 3
+    // iter all data in sysEx
+    while (i<buffersize) {
+       // Start of frame
+       if(bigSysEx[i] == 0xF0){
+          //frames[frameCpt] = malloc(sizeof(t_frame));
+           frames[frameCpt].start = i;
+           frameCpt++;
+       }
+    
+       // End of frame
+       if(bigSysEx[i] == 0xF7){
+            frames[frameCpt-1].size = 1+i-start ;
+            frames[frameCpt-1].data = malloc(frames[frameCpt-1].size);
+            memcpy(frames[frameCpt-1].data, bigSysEx + frames[frameCpt-1].start, frames[frameCpt-1].size);           
+
+            printf(" Frame #%d |   start=%d  i=%d   \n",frameCpt,start, i);
+            start = i+1;
+       }
+       // Display it 
+       //printf("0x%X.",bigSysEx[i]);
+       i++;
+    }
+
+    printf("\n --------------------- \n");
+
+    int j=0;
+    for(j=0; j<frameCpt;j++){
+        printf("|%d (size:%d)| %s|  \n", j, frames[j].size, frames[j].data + 7);
+    }
+
+    printf("\n --------------------- \n ");
+
+
     return(0);
 }
 
